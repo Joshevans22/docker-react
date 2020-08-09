21da309beac50de0a6b3a9fafeb3d7db490ceb76
Joshua Evans <evansj2205@gmail.com>
2020-08-09 13:03:33 +0100

    testing build system

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello der! It works
+          Hello! It works.
         </p>
         <a
           className="App-link"
